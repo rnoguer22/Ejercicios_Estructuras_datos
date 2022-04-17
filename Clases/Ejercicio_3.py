ae69111d7f414df6947134d1873baa3cba6b5637
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2022-04-17 12:03:36 +0200

    Enunciado del tercer ejercicio

---
+++ Clases/Ejercicio_3.py
@@ -0,0 +1,9 @@
+producto = Producto(Naturaleza.ALIMENTARIA) # IVA 5,5% 
+precio_neto = FactoryFactura.crear(producto).facturar() 
+print(precio_neto) 
+# 105.5 
+ 
+producto = Producto(Naturaleza.SERVICIO) # IVA 20% 
+precio_neto = FactoryFactura.crear(producto).facturar() 
+print(precio_neto) 
+# 120 
