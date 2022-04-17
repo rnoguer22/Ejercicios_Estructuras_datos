1f2d2a425f3d3568c0aa9379713155922ada0f90
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2022-04-17 12:57:01 +0200

    Comprobacion de que las lineas de texto son string

--- Clases/Ejercicio_2.py
+++ Clases/Ejercicio_2.py
@@ -1,8 +1,16 @@
+from multiprocessing.sharedctypes import Value
+import string
+
+
 class MVC:
     #Constructor
     def __init__(self, linea1, linea2):
         self.linea1 = linea1
         self.linea2 = linea2
         #Convertimos las dos lineas de texto a mayusculas
-        self.linea1.upper()
-        self.linea2.upper()
+        #Ademas, nos aseguramos que ambas lineas de texto sean cadenas de texto
+        if isinstance(self.linea1 and self.linea2, string):
+            self.linea1.upper()
+            self.linea2.upper()
+        else:
+            raise ValueError("Las lineas deben de ser cadenas de texto")
