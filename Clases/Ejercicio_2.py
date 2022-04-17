2d8b59a2c7c4c0d763ae7e3118ebc30b41ee072a
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2022-04-17 12:52:56 +0200

    Pasamos las lineas de texto a mayusculas

--- Clases/Ejercicio_2.py
+++ Clases/Ejercicio_2.py
@@ -3,3 +3,6 @@ class MVC:
     def __init__(self, linea1, linea2):
         self.linea1 = linea1
         self.linea2 = linea2
+        #Convertimos las dos lineas de texto a mayusculas
+        self.linea1.upper()
+        self.linea2.upper()
