d28f5c93022ea6d4d3fb0cf744eeef91840d122f
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2022-04-17 12:45:02 +0200

    Cambios en las clases FactoryFactura y Producto

--- Clases/Ejercicio_3.py
+++ Clases/Ejercicio_3.py
@@ -9,26 +9,28 @@ class Producto(Naturaleza):
     #Constructor
     def __init__(self, tasa_iva):
         self.tasa_iva = tasa_iva
-    
+
+    def facturar(self):
+        return 100 + 100*self.tasa_iva
+
+
 class FactoryFactura(Producto):
     #Constructor
     def __init__(self):
         pass
 
-    def crear(self, factura):
-        resultado = 100 + self.tasa_iva*100
-    
-    def facturar(self):
-        print(self.crear(self.factura))
+    def crear(self):
+        clase = Producto(self.tasa_iva)
+        return clase
 
 
-
+Naturaleza = Naturaleza()
 
 producto = Producto(Naturaleza.ALIMENTARIA) # IVA 5,5% 
-precio_neto = FactoryFactura.crear(producto).facturar() 
+precio_neto = FactoryFactura.crear(producto).facturar()
 print(precio_neto) 
 # 105.5 
- 
+
 producto = Producto(Naturaleza.SERVICIO) # IVA 20% 
 precio_neto = FactoryFactura.crear(producto).facturar() 
 print(precio_neto) 
