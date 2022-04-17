2b38379c2146e0754440f13cef54b6aa2ac1955b
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2022-04-17 12:57:45 +0200

    Encapsulamiento

--- Clases/Ejercicio_2.py
+++ Clases/Ejercicio_2.py
@@ -5,12 +5,14 @@ import string
 class MVC:
     #Constructor
     def __init__(self, linea1, linea2):
-        self.linea1 = linea1
-        self.linea2 = linea2
+        self.__linea1 = linea1
+        self.__linea2 = linea2
         #Convertimos las dos lineas de texto a mayusculas
         #Ademas, nos aseguramos que ambas lineas de texto sean cadenas de texto
         if isinstance(self.linea1 and self.linea2, string):
-            self.linea1.upper()
-            self.linea2.upper()
+            self.__linea1.upper()
+            self.__linea2.upper()
         else:
             raise ValueError("Las lineas deben de ser cadenas de texto")
+        
+
