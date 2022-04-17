566306399b8f8bca4f9f4824db4e7a3b76dc23e4
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2022-04-17 12:07:39 +0200

    Definimos  la clase Producto., asi como su constructor

--- Clases/Ejercicio_3.py
+++ Clases/Ejercicio_3.py
@@ -1,3 +1,13 @@
+class Producto:
+
+    def __init__(self, tasa_iva):
+        self.tasa_iva = tasa_iva
+    
+    
+
+
+
+
 producto = Producto(Naturaleza.ALIMENTARIA) # IVA 5,5% 
 precio_neto = FactoryFactura.crear(producto).facturar() 
 print(precio_neto) 
